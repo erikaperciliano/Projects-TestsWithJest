4db415d2574362008914713e82515cf5bcef4af6
erika.perciliano <erika.perciliano@farfetch.com>
2023-10-07 14:53:22 +0100

    introducing mock functions

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -17,14 +17,9 @@ test('it shows two inputs and a button', () => {
 });
 
 test('its calls onUserAdd when the form is submitted', () => {
-    //NOT THE BEST IMPLEMENTATION
-    const argList = []
-    const callback = (...args) => {
-        argList.push(args)
-    }
-
+    const mock = jest.fn();
     // Try to render my component
-    render(<UserForm onUserAdd={callback}/>);
+    render(<UserForm onUserAdd={mock}/>);
 
     // Find the two inputs
     const [ nameInput, emailInput ] = screen.getAllByRole('textbox');
@@ -44,6 +39,9 @@ test('its calls onUserAdd when the form is submitted', () => {
     user.click(button)
 
     // Assertion to make sure 'onUserAdd' get called with email/name
-    expect(argList).toHaveLength(1)
-    expect(argList[0][0]).toEqual({name: 'Erika', email:'erika@gmail.com'})
+    expect(mock).toHaveBeenCalled()
+    expect(mock).toHaveBeenLastCalledWith({
+        name: 'Erika', 
+        email:'erika@gmail.com'
+    })
 })
