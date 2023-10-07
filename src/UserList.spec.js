62a86b7957a5f14bf018434c23261fda2dad3aee
erika.perciliano <erika.perciliano@farfetch.com>
2023-10-07 16:03:01 +0100

    query function scape hatches

--- src/UserList.spec.js
+++ src/UserList.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from './UserList';
 
 it('render one row per user', () => {
@@ -10,9 +10,10 @@ it('render one row per user', () => {
     render(<UserList users={users}/>)
 
     // Find all the rows in the table
-    screen.logTestingPlaygroundURL()
+    const rows = within(screen.getByTestId('users')).getAllByRole('row')
 
     // Assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2)
 })
 
 it('render the email and  name of each user', () => {
