ed3710c7e8eb2ad27c88165a73a9c92fceb3aa56
erika.perciliano <erika.perciliano@farfetch.com>
2023-10-07 17:32:16 +0100

    testing the whole app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from '@testing-library/user-event';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("can receive a new user and show it on a list", async() => {
+  render(<App />)
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i })
+  const emailInput = screen.getByRole("textbox", { name: /email/i })
+  const button = screen.getByRole("button");
+
+  await user.click(nameInput);
+  await user.keyboard("erika");
+
+  await user.click(emailInput);
+  await user.keyboard("erika@gmail.com")
+
+  await user.click(button);
+
+  const name = screen.getByRole("cell", { name: "erika" });
+  const email = screen.getByRole("cell", { name: "erika@gmail.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+})
