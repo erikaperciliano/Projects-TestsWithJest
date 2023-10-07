5b5685159c4a2d4fea5f62a1fd592fb0e650529b
erika.perciliano <erika.perciliano@farfetch.com>
2023-10-07 16:27:31 +0100

    avoiding BeforeEach

--- src/UserList.spec.js
+++ src/UserList.spec.js
@@ -1,13 +1,22 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from './UserList';
 
-it('render one row per user', () => {
-    // Render the component
+function renderComponent(){
     const users = [
         { name: 'erika', email: 'erika@gmail.com' },
-        { name: 'raquel', email: 'raquel@gmail.com '}
+        { name: 'raquel', email: 'raquel@gmail.com' }
     ]
     render(<UserList users={users}/>)
+
+    return {
+        users
+    }
+}
+
+
+it('render one row per user', () => {
+    // Render the component
+    renderComponent()
 
     // Find all the rows in the table
     const rows = within(screen.getByTestId('users')).getAllByRole('row')
@@ -17,11 +26,7 @@ it('render one row per user', () => {
 })
 
 it('render the email and  name of each user', () => {
-    const users = [
-        { name: 'erika', email: 'erika@gmail.com' },
-        { name: 'raquel', email: 'raquel@gmail.com' }
-    ]
-    render(<UserList users={users}/>)
+    const { users } = renderComponent()
 
     for(let user of users) {
         const name = screen.getByRole('cell', { name: user.name });
