12ce567375ccffb277b9d6d568cc31cd46d44281
erika.perciliano <erika.perciliano@farfetch.com>
2023-10-12 09:53:36 +0100

    add looping over assertion at test

--- codesplain-starter/src/components/repositories/RepositorySumary.test.js
+++ codesplain-starter/src/components/repositories/RepositorySumary.test.js
@@ -14,3 +14,24 @@ it('displays the primary language  of the repository ', () => {
     const language = screen.getByText(/python/i);
     expect(language).toBeInTheDocument();
 })
+
+it('displays information  about the repository ', () => {
+    const repository = {
+        stargazers_count: 4,
+        open_issues: 5,
+        forks: 3,
+        language: 'python'
+    }
+
+    render(<RepositoriesSummary repository={repository}/>);
+
+    for(let key in repository){
+        const value = repository[key]
+        const element = screen.getByText(new RegExp(value));
+
+        expect(element).toBeInTheDocument();
+    }
+
+    const language = screen.getByText(/python/i);
+    expect(language).toBeInTheDocument();
+})
